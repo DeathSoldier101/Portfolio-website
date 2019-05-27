0df8424000615222bd9c43ebd687105ef053c22e
DeathSoldier101 <51077124+DeathSoldier101@users.noreply.github.com>
2019-05-27 10:45:40 +0200

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,38 @@
+let f1= document.querySelector(".f1");
+let f2= document.querySelector(".f2");
+let f3= document.querySelector(".f3");
+let f4= document.querySelector(".f4");
+let f5= document.querySelector(".f5");
+let f6= document.querySelector(".f6");
+
+f1.addEventListener("click",function () {
+  console.log("test");
+  alert("Opdracht voor GD2, waar we een poster moesten maken voor onze eigen film. Hier moesten we zelf beeld materiaal maken. Ik wou zelf hier alles illustratief doen. Het resultaat was beter dan ik zelf had verwacht en ik ben er best trots op.")
+});
+
+f2.addEventListener("click",function () {
+  alert("Dit is een poster voor OP2, waar je zelf poters moest maken voor je eigen actiegroep. Wij deden het rond food waste.")
+});
+
+f3.addEventListener("click",function () {
+  alert("Dit is een poster voor OP2, waar je zelf poters moest maken voor je eigen actiegroep. Wij deden het rond food waste.")
+});
+
+f4.addEventListener("click",function () {
+  alert("Dit is een poster voor OP2, waar je zelf poters moest maken voor je eigen actiegroep. Wij deden het rond food waste.")
+});
+
+f5.addEventListener("click",function () {
+  alert("Opdracht voor SW2. Dit heb ik gemaakt in de laatste les van Photoshop. We moesten het voorbeeld zo goed mogelijk namaken. Het was bijna exact het elfde als het voorbeeld. Het is dus zeer goed gelukt.")
+});
+
+f6.addEventListener("click",function () {
+  alert("Een illustratie die ik heb gemaakt voor OP2 om in onze video te gebruiken. Deze video heb ik zelf helemaal geanimeerd. Dit was iets heel miniem maar ik vond het zeer goed gelukt.")
+});
+
+let omhoog = document.querySelector(".boven");
+
+omhoog.addEventListener("click",function () {
+  console.log("test2");
+  window.scrollTop;
+});
---
+++ script.js
@@ -0,0 +1,9 @@
+// hier komt je code
+// console.log("Hallo wereld!");
+
+let bedankt = document.querySelector("button");
+
+bedankt.addEventListener('click',function () {
+	console.log("test");
+	alert("Bedankt voor het kijken naar mijn website!")
+})
